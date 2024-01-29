a5bcd1d90961bb639791465783cfc5e4e2c1f5fc
Rashi Gupta <guptarashi98266@gmail.com>
2024-01-29 11:27:51 +0530

    jhdn

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
     <div className="max-w-[1520px] m-auto px-4 py-2 bg-[#24262b]">
       <div className="py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
         <div>
-          <h1 className="w-full text-3xl font-bold text-orange-500">YumEats</h1>
+          <h1 className="w-full text-3xl font-bold text-orange-500">RashiBite</h1>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit
             ullam iste repellat consequatur libero reiciendis, blanditiis
